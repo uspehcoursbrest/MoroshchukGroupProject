d54897410dbf7c1af38d10bd00f348e5d0e8cc01
EgorHubBrest <44066689+EgorHubBrest@users.noreply.github.com>
2018-11-20 23:26:28 +0300

    б

--- js/script.js
+++ js/script.js
@@ -1,39 +1,54 @@
 $(document).ready(function () {
-    $('.card').mouseover(function () {
-        $(this).find('.thumbnail').stop().fadeIn()
+    $(".card").mouseover(function () {
+        $(this).find(".thumbnail").stop().fadeIn()
     });
-    $('.card').mouseout(function () {
-        $(this).find('.thumbnail').stop().fadeOut()
+    $(".card").mouseout(function () {
+        $(this).find(".thumbnail").stop().fadeOut()
     });
-    var accord = $('.accordion');
-    accord.find('.title:not(.active)').siblings('div').slideUp();
-    accord.find('.title').on('click', function () {
-        $(this).siblings('div').stop().slideToggle(500);
-        if ($(this).hasClass('active')) {
-            $(this).removeClass('active');
+
+    var accord = $(".accordion");
+    accord.find(".title:not(.active)").siblings("div").slideUp();
+    accord.find(".title").on("click", function () {
+        $(this).siblings("div").stop().slideToggle(500);
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
         }
         else {
-            $(this).addClass('active');
+            $(this).addClass("active");
+        }
+    });
+
+    var btnUp =$(".up");
+    btnUp.hide();
+    $(window).scroll(function () {
+        if($(this).scrollTop() > 50){
+            btnUp.fadeIn();
+        } else {
+            btnUp.fadeOut();
+
         }
 
     })
-    var btnUp = $('.up');
-    btnUp.hide();
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            btnUp.fadeIn();
-        }
-        else {
-            btnUp.fadeOut();
-        }
-    })
     btnUp.click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 500)
+        $("body,html").animate({
+            scrollTop:0
+        }, 500);
         return false;
+    })
+    $(".mainSlider").owlCarousel({
+        dots: true,
+        nav: false,
+        items: 1,
+        loop:true,
+        dotsContainer: "#customDots"
     });
-});
+
+    $("#tabs").tabs({
+        active: 0
+    });
+
+
+})
 
 
 
